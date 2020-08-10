5370150b72ac1044acf9692bb66d2946bccfe0e0
zzp <i@mezzp.com>
2020-08-10 19:35:24 +0800

    常量Demo

---
+++ basic_code/入门程序、常量、变量/src/com/mezzp/day01/ConstDemo.java
@@ -0,0 +1,36 @@
+package com.mezzp.day01;
+
+/**
+ * @author zzp
+ * @version 1.0
+ * @date 2020/8/10 19:31
+ * FileName: ConstDemo
+ * Description: 常量
+ */
+public class ConstDemo {
+    public static void main(String[] args) {
+        // 字符串常量
+        System.out.println("ABC");
+        System.out.println("");
+        // 字符串两个双引号中间的内容为空
+        System.out.println("XYZ");
+
+        // 整数常量
+        System.out.println(30);
+        System.out.println(-500);
+
+        // 浮点数常量（小数）
+        System.out.println(3.14);
+        System.out.println(-2.5);
+
+        // 字符常量
+        System.out.println('A');
+        System.out.println('6');
+        // System.out.println('');
+        // 两个单引号中间必须有且仅有一个字符
+
+        //布尔常量
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
