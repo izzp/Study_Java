5194d06ed581ac586d88aba334d844194ff59882
zzp <i@mezzp.com>
2020-08-10 19:10:28 +0800

    :art: 重新规划仓库

---
+++ basic_code/入门程序、常量、变量/src/com/mezzp/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mezzp.day01;
+
+/**
+ * @author zzp
+ * @version 1.0
+ * @date 2020/8/10 19:05
+ * FileName: HelloWorld
+ * Description: HelloWorld
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
